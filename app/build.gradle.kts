756bd3890f6bc7b4ffeb7cba33c4df273a7097bd
nicourrrn <s57111702@gmail.com>
2023-10-24 22:18:20 +0300

    Add nav bar dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,8 @@ android {
 }
 
 dependencies {
-
+    val nav_version = "2.7.4"
+    implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
