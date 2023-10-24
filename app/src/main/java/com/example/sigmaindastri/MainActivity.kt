c7de96506bd8118ebde17bd1946628bc12588dc6
nicourrrn <s57111702@gmail.com>
2023-10-24 22:31:38 +0300

    Add nav bar example

--- app/src/main/java/com/example/sigmaindastri/MainActivity.kt
+++ app/src/main/java/com/example/sigmaindastri/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.sigmaindastri.ui.theme.SigmaIndastriTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +35,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "index") {
+        composable("auth") { AuthView() }
+    }
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
+    Button(onClick = { navController.navigate("auth") }) {
+        Text(text = "To auth")
+    }
 }
 
 @Preview(showBackground = true)
@@ -43,4 +54,11 @@ fun GreetingPreview() {
     SigmaIndastriTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun AuthView(){
+    Text(
+        text = "Hello from auth view"
+    )
 }
