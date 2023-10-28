d4a3487de96347801bbf015b4e1e6f9cf58da4e8
pustostas <staska.golka@gmail.com>
2023-10-28 05:05:09 +0300

    finished registration view

--- app/src/main/java/com/example/sigmaindastri/MainActivity.kt
+++ app/src/main/java/com/example/sigmaindastri/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.widget.DatePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
@@ -32,6 +36,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.sigmaindastri.ui.theme.SigmaIndastriTheme
+import java.lang.reflect.Array.get
 import java.util.Calendar
 import java.util.Date
 
@@ -47,7 +52,12 @@ class MainActivity : ComponentActivity() {
                     val navController = rememberNavController()
                     val stateManager by remember { mutableStateOf(StateManager(navController, "")) }
                     NavHost(navController = navController, startDestination = Route.Index.url) {
-                        composable(Route.Index.url) { Greeting(stateManager = stateManager,name = "Vadym sosi bibijon") }
+                        composable(Route.Index.url) {
+                            Greeting(
+                                stateManager = stateManager,
+                                name = "Vadym sosi bibijon"
+                            )
+                        }
                         composable(Route.Login.url) { LoginView() }
                         composable(Route.Registration.url) { RegistrationView() }
                     }
@@ -56,14 +66,15 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
 fun Greeting(stateManager: StateManager, name: String) {
 
-    Column (
+    Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-    ){
+    ) {
         Button(
             onClick = { stateManager.navController.navigate("login") },
             modifier = Modifier
@@ -86,33 +97,35 @@ fun Greeting(stateManager: StateManager, name: String) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginView(){
-    var email by remember { mutableStateOf("")}
-    var password by remember { mutableStateOf("")}
+fun LoginView() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
     var isValidEmail by remember { mutableStateOf(false) }
     var isValidPassword by remember { mutableStateOf(false) }
     val emailRequiredChars = setOf('@', '.')
-    Column (
+    Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(text = "Hello from login view ${email}")
-        OutlinedTextField(value = email,
+        OutlinedTextField(
+            value = email,
             onValueChange = { input ->
                 email = input
                 isValidEmail = input.isNotEmpty() && input.all(emailRequiredChars::contains)
             },
             label = { Text("Email") },
         )
-        OutlinedTextField(value = password,
+        OutlinedTextField(
+            value = password,
             onValueChange = { input ->
                 password = input
                 isValidPassword = input.length >= 6
             },
             label = { Text("Password") },
         )
-        Button(onClick = {  }, enabled = isValidEmail && isValidPassword) {
+        Button(onClick = { }, enabled = isValidEmail && isValidPassword) {
             Text(text = "Log in", fontSize = 20.sp)
         }
     }
@@ -120,60 +133,69 @@ fun LoginView(){
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RegistrationView(){
-    var email by remember { mutableStateOf("")}
-    var password by remember { mutableStateOf("")}
-    var firstName by remember { mutableStateOf("")}
-    var lastName by remember { mutableStateOf("")}
+fun RegistrationView() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var firstName by remember { mutableStateOf("") }
+    var lastName by remember { mutableStateOf("") }
     var isValidEmail by remember { mutableStateOf(false) }
     var isValidPassword by remember { mutableStateOf(false) }
     var isValidFirstName by remember { mutableStateOf(false) }
     var isValidLastName by remember { mutableStateOf(false) }
     val emailRequiredChars = setOf('@', '.')
+    var isMale by remember { mutableStateOf(false) }
+    var isServiceProvider by remember { mutableStateOf(false) }
+    val dateOfBirth = remember { mutableStateOf("") }
 
     val mContext = LocalContext.current
-
     val mCalendar = Calendar.getInstance()
     val mYear = mCalendar.get(Calendar.YEAR)
     val mMonth = mCalendar.get(Calendar.MONTH)
     val mDay = mCalendar.get(Calendar.DAY_OF_MONTH)
     mCalendar.time = Date()
-    val mDate = remember { mutableStateOf("") }
+
+
+
     val mDatePickerDialog = DatePickerDialog(
         mContext,
         { _: DatePicker, mYear: Int, mMonth: Int, mDayOfMonth: Int ->
-            mDate.value = "$mDayOfMonth/${mMonth+1}/$mYear"
+            dateOfBirth.value = "$mDayOfMonth/${mMonth + 1}/$mYear"
         }, mYear, mMonth, mDay
     )
 
 
-    Column (
+    Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        OutlinedTextField(value = email,
+        OutlinedTextField(
+            value = email,
             onValueChange = { input ->
                 email = input
                 isValidEmail = input.isNotEmpty() && input.all(emailRequiredChars::contains)
             },
             label = { Text("Email") },
         )
-        OutlinedTextField(value = password,
+        OutlinedTextField(
+            value = password,
             onValueChange = { input ->
                 password = input
                 isValidPassword = input.length >= 6
             },
             label = { Text("Password") },
         )
-        OutlinedTextField(value = firstName,
+        OutlinedTextField(
+            value = firstName,
             onValueChange = { input ->
                 firstName = input
                 isValidFirstName = input.isNotEmpty()
             },
             label = { Text("First Name") },
         )
-        OutlinedTextField(value = lastName,
+
+        OutlinedTextField(
+            value = lastName,
             onValueChange = { input ->
                 lastName = input
                 isValidLastName = input.isNotEmpty()
@@ -182,10 +204,58 @@ fun RegistrationView(){
         )
 
         Button(onClick = { mDatePickerDialog.show() }) {
-            Text(text = "Open Date Picker", color = Color.White)
+            Text(text = "Pick birth date: $mYear/$mMonth/$mDay", color = Color.White)
+        }
+        Row()
+        {
+            Text("Choose your sex")
+            Button(
+                onClick = {
+                    isMale = true
+                },
+                colors = ButtonDefaults.buttonColors(containerColor = if (isMale) Color(0xFF6650a4) else Color.Gray)
+            ) {
+                Text("Male")
+            }
+            Button(
+                onClick = { isMale = false },
+                colors = ButtonDefaults.buttonColors(containerColor = if (!isMale) Color(0xFF6650a4) else Color.Gray)
+            ) {
+                Text("Female")
+            }
         }
 
-        Button(onClick = {  }, enabled = isValidEmail && isValidPassword && isValidLastName && isValidFirstName) {
+        Row()
+        {
+            Text("Choose your role")
+            Button(
+                onClick = { isServiceProvider = false },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = if (!isServiceProvider) Color(
+                        0xFF6650a4
+                    ) else Color.Gray
+                )
+            ) {
+
+                Text("User")
+            }
+            Button(
+                onClick = { isServiceProvider = true },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = if (isServiceProvider) Color(
+                        0xFF6650a4
+                    ) else Color.Gray
+                )
+            ) {
+
+                Text("Service provider")
+            }
+        }
+
+        Button(
+            onClick = { },
+            enabled = isValidEmail && isValidPassword && isValidLastName && isValidFirstName
+        ) {
             Text(text = "Sign up", fontSize = 20.sp)
         }
     }
